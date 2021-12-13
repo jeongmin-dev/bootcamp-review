227acc0fd2748672e3bd990e5f4df536c390df9f
John <john3210of@gmail.com>
2021-12-13 23:23:44 +0900

    bone

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
-# client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://test:test@localhost', 27017)
-db = client.review_db
-# db1 = client.review_db
+client = MongoClient('localhost', 27017)
+db1 = client.review_db
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+# db = client.review_db
+
 
 @app.route('/')
 def review_main():
