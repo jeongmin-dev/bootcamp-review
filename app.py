93361450cda038fcc6225ef12e16f535f98e1cb5
JeongminLee0908 <72373005+JeongminLee0908@users.noreply.github.com>
2021-12-18 21:30:11 +0900

    Add files via upload
    기본 폴더 및 파일 세팅

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request, session
 app = Flask(__name__)
 
-from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db1 = client.review_db
 # client = MongoClient('mongodb://test:test@localhost', 27017)
@@ -12,5 +12,21 @@ db1 = client.review_db
 def review_main():
     return render_template('index.html')
 
+@app.route('/login')
+def review_login():
+    return render_template('login.html')
+
+@app.route('/register')
+def review_register():
+    return render_template('register.html')
+
+@app.route('/review')
+def review_write():
+    return render_template('review.html')
+
+@app.route('/mypage')
+def review_mypage():
+    return render_template('mypage.html')
+
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
