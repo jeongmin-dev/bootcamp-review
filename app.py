6070ff9e03a7695bbf1acd79d6e04210f9dfd707
JeongminLee0908 <72373005+JeongminLee0908@users.noreply.github.com>
2021-12-18 16:35:05 +0900

    Add files via upload
    메인 헤더, 푸더 변경 및 로그인, 회원가입 페이지 변경

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from flask import Flask, render_template, jsonify, request, session
 app = Flask(__name__)
 
-from pymongo import MongoClient
-# client = MongoClient('localhost', 27017)
-client = MongoClient('mongodb://test:test@localhost', 27017)
-db = client.review_db
-# db1 = client.review_db
+client = MongoClient('localhost', 27017)
+db1 = client.review_db
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+# db = client.review_db
+
 
 @app.route('/')
 def review_main():
     return render_template('index.html')
 
+@app.route('/login')
+def review_login():
+    return render_template('login.html')
+
+@app.route('/register')
+def review_register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
